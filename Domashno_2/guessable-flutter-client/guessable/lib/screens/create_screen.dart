dff3382a5b0feb58d0958258d3f0899b3a4d72c9
MetoA <metoandov@gmail.com>
2023-03-02 22:07:22 +0100

    - fixed position of camera capture button - added button to toggle front and back camera - now showing taken image on the page - now showing a google map of the user's location - fixed a bug where

--- Domashno_2/guessable-flutter-client/guessable/lib/screens/create_screen.dart
+++ Domashno_2/guessable-flutter-client/guessable/lib/screens/create_screen.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:guessable/api/location_api.dart';
 import 'package:guessable/blocs/create_guess_bloc/create_guess_bloc.dart';
 import 'package:guessable/blocs/create_guess_bloc/create_guess_state.dart';
 
@@ -21,60 +26,129 @@ class CreateScreen extends StatefulWidget {
 class _CreateScreenState extends State<CreateScreen> {
   Position? userPosition;
   String? userPositionError;
+  final Completer<GoogleMapController> _mapController = Completer<GoogleMapController>();
+
+  bool locationLoading = false;
 
   @override
   void initState() {
-    print(_determinePosition());
+    _determinePosition();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Create'),
-      ),
-      body: _body(context),
-      bottomNavigationBar: const BottomNav(
-        currentRoute: CreateScreen.route,
-      ),
-    );
-  }
-
-  Widget _body(BuildContext context) {
-    BlocProvider.of<CreateGuessBloc>(context).add(CreateGuessInitializedEvent());
-    print('Initial state called');
     return BlocBuilder<CreateGuessBloc, CreateGuessState>(builder: (context, state) {
-      // TODO current issue where after taking picture and getting redirected here, the image is null until maybe refresh or navigate to other place
       final state = context.watch<CreateGuessBloc>().state;
-      return Container(
-        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 50),
-        child: Center(
-            child: Column(
-          children: [
-            Text('Location is: $userPosition'),
-            ElevatedButton(
-              onPressed: () async {
-                availableCameras().then((value) =>
-                    Navigator.push(context, MaterialPageRoute(builder: (_) => CameraScreen(cameras: value))));
-              },
-              child: Text('Take a picture'),
-            ),
-            Text(state is CreateGuessPopulatedState ? 'HAS IMAGE ${state.locationImage?.name}' : 'NO IMAGE')
-          ],
-        )),
+      return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Create'),
+        ),
+        body: _body(context, state.locationImage),
+        floatingActionButton: Column(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
+          FloatingActionButton(
+            onPressed: () async {
+              availableCameras().then(
+                  (value) => Navigator.push(context, MaterialPageRoute(builder: (_) => CameraScreen(cameras: value))));
+            },
+            child: const Icon(Icons.camera_alt_outlined),
+          ),
+          const SizedBox(height: 20),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: userPosition != null && state.locationImage != null
+                ? () async {
+                    LocationAPI.createLocation(
+                        userPosition!.latitude, userPosition!.longitude, await state.locationImage!.readAsBytes());
+                  }
+                : null,
+            child: const Icon(Icons.check),
+          ),
+        ]),
+        bottomNavigationBar: const BottomNav(
+          currentRoute: CreateScreen.route,
+        ),
       );
     });
+  }
+
+  Widget _body(BuildContext context, XFile? locationImage) {
+    BlocProvider.of<CreateGuessBloc>(context).add(CreateGuessInitializedEvent());
+    return SingleChildScrollView(
+        child: Container(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20, top: 50),
+      child: Center(
+          child: Column(
+        children: [
+          _imageWidget(locationImage),
+          const SizedBox(height: 20),
+          _mapsWidget(),
+        ],
+      )),
+    ));
+  }
+
+  Widget _imageWidget(XFile? image) {
+    return image != null
+        ? Container(decoration: _borderBoxDecoration(), child: Image.file(File(image.path)))
+        : const Text('You need to take a picture before submitting!', style: TextStyle(color: Colors.red));
+  }
+
+  Widget _mapsWidget() {
+    if (!locationLoading) {
+      return userPosition != null
+          ? Container(
+              decoration: _borderBoxDecoration(),
+              height: 200,
+              child: GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition:
+                    CameraPosition(target: LatLng(userPosition!.latitude, userPosition!.longitude), zoom: 10),
+                onMapCreated: (GoogleMapController controller) {
+                  _mapController.complete(controller);
+                },
+                markers: _markers().toSet(),
+              ))
+          : const Text('You need to allow location before submitting!', style: TextStyle(color: Colors.red));
+    } else {
+      return Column(
+        children: const [CircularProgressIndicator(), SizedBox(height: 10), Text('Loading location...')],
+      );
+    }
+  }
+
+  BoxDecoration _borderBoxDecoration() {
+    return BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(5)),
+        border: Border.all(color: Colors.deepPurple, width: 2));
+  }
+
+  List<Marker> _markers() {
+    if (userPosition != null) {
+      return [
+        Marker(
+            markerId: MarkerId(userPosition!.toString()),
+            position: LatLng(userPosition!.latitude, userPosition!.longitude),
+            icon: BitmapDescriptor.defaultMarker)
+      ];
+    } else {
+      return [];
+    }
   }
 
   _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
+    setState(() {
+      locationLoading = true;
+    });
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       setState(() {
         userPositionError = 'Location services are disabled.';
+        locationLoading = false;
       });
     }
 
@@ -84,6 +158,7 @@ class _CreateScreenState extends State<CreateScreen> {
       if (permission == LocationPermission.denied) {
         setState(() {
           userPositionError = 'Location permissions are denied';
+          locationLoading = false;
         });
       }
     }
@@ -91,14 +166,14 @@ class _CreateScreenState extends State<CreateScreen> {
     if (permission == LocationPermission.deniedForever) {
       setState(() {
         userPositionError = 'Location permissions are permanently denied, we cannot request permissions.';
+        locationLoading = false;
       });
     }
 
     var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
     setState(() {
       userPosition = position;
+      locationLoading = false;
     });
-    print('USER POSITION ${userPosition?.toString() ?? ''}');
-    print('USER POSITION ERROR ${userPositionError ?? ''}');
   }
 }
